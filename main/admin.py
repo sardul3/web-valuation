fb64d8890cbc67da6fb5c8624ad8a4ece7ac0989
Sagar Poudel <poudels@warhawks.ulm.edu>
2019-02-13 15:39:10 -0600

    Add new models and register them to admin page

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Evaluator
+from .models import (Evaluator, Administrator,
+                    Rubric, Objective, Measure)
 
 # Register your models here.
 admin.site.register(Evaluator)
+admin.site.register(Administrator)
+admin.site.register(Rubric)
+admin.site.register(Objective)
+admin.site.register(Measure)
--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.mail import send_mail
+from datetime import datetime
 
 # Create your models here.
 
@@ -20,3 +21,35 @@ class Evaluator(models.Model):
 
     def __str__(self):
         return self.name
+
+class Administrator(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+class Measure(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+
+class Objective(models.Model):
+    evaluator = models.ForeignKey(Evaluator, related_name='evaluator', on_delete = models.CASCADE)
+    text = models.TextField()
+    measure = models.ForeignKey(Measure, on_delete= models.CASCADE, related_name='measure')
+
+    def __str__(self):
+        return self.text
+
+
+class Rubric(models.Model):
+    created_at = models.DateTimeField(default = datetime.now)
+    created_by = models.ForeignKey(Administrator, related_name='administrator', on_delete = models.CASCADE)
+    number_of_rows = models.PositiveIntegerField()
+    number_of_cols = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.created_by
