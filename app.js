95d2691daf3e5f2ab3a215438072ec2a111087c8
nadhakari670 <31519668+nadhakari670@users.noreply.github.com>
2019-02-24 20:09:00 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+function addRow() {
+  var tbl = document.getElementById("rubricbase");
+  var row = tbl.insertRow(tbl.rows.length);
+  var i; 
+  for (i=0; i<tbl.rows[0].cells.length; i++){
+      createCell(row.insertCell(i),'row');
+  }
+}
+
+function createCell(cell, style){
+    var tablecell = document.createElement('td');
+    
+    tablecell.setAttribute('style','width:10%' );
+    cell.appendChild(tablecell);
+    tablecell.contentEditable = 'true';
+}
+
+function addColumn(){
+    var tbl = document.getElementById("rubricbase");
+    var i;
+    for (i=0; i<tbl.rows.length; i++){
+        createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length-1),'contentEditable=true;');
+    }
+}
+
+function deleteColumns(){
+    var tbl = document.getElementById("rubricbase");
+    var totalCols = tbl.rows[0].cells.length-1;
+    var i;
+     
+    for(i=0; i<tbl.rows.length; i++){
+        if(totalCols>=2){
+            tbl.rows[i].deleteCell(totalCols-1);
+        }
+     
+    }
+    
+
+}
+
+function deleteRows(){
+    var tbl = document.getElementById("rubricbase");
+    var lastRow = tbl.rows.length-1;
+    if(lastRow>1){
+        tbl.deleteRow(lastRow);
+    }
+    else{
+        document.getElementById("deleteRows").disabled = true;
+    }
+
+    
+}
