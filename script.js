ce6e8ebdb8d27bc2a3ba4411aad6121ef4f747c3
nadhakari670 <31519668+nadhakari670@users.noreply.github.com>
2019-04-01 13:30:18 -0500

    Updated the rubric skeleton
    Styles added and updates javascript file

---
+++ script.js
@@ -0,0 +1,41 @@
+	function createTable()
+	{
+		var num_rows = document.getElementById('rows').value;
+		var num_cols = document.getElementById('cols').value;
+		var theader = '<table style="width:100%" class = "table table-bordered">\n';
+		var tbody = '';
+
+		for( var i=0; i<num_rows;i++)
+		{
+			tbody += '<tr>';
+			for( var j=0; j<num_cols;j++)
+			{
+				if(i==0 && j==0){
+					tbody += '<th width="30%">';
+					tbody += 'Criteria';
+					tbody += '</th>';
+				}
+				else if(i==0 && j>0 && j<num_cols-1){
+					tbody += '<th>'
+					tbody += j;
+					tbody += '</th>';
+				}
+				else if(i==0 && j==num_cols-1){
+					tbody += '<th>';
+					tbody += 'Score';
+					tbody += '</th>';
+				}
+				else{
+				tbody += '<td height = "40%" contenteditable>';
+				tbody += ""
+				tbody += '</td>'
+				}
+			}
+			tbody += '</tr>\n';
+		}
+		var tfooter = '</table>';
+		var buttn = '<button class="btn btn-primary btn-lg  ">Add Rubric</button>';
+		document.getElementById('rubricframe').innerHTML = theader + tbody + tfooter;
+		document.getElementById('addrubric').innerHTML = buttn;
+		
+	}
